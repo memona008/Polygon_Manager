9637ad13ff33d431c6a322e7dd15746f12b56673
Memona Sultan <43179211+memona008@users.noreply.github.com>
2021-11-27 18:32:34 +0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Memona S',
   author_email = 'memonasultan54@gmail.com',
   url = 'https://github.com/memona008/Polygon_Manager',
-  download_url = 'https://github.com/memona008/Polygon_Manager/archive/refs/tags/v_01.tar.gz',
+  download_url = 'https://github.com/memona008/Polygon_Manager/archive/refs/tags/v_011.tar.gz',
   keywords = ['polygon', 'video', 'draw polygon', 'box', 'parallelogram', 'image', 'manager', 'capture','points'],
   install_requires=[
           'numpy',
