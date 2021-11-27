20516fc1d413e427d6bb81537942074f1bf556c1
MemonaS <memonasultan54@gmail.com>
2021-11-27 18:00:45 +0500

    Added Package Files

---
+++ README.md
@@ -0,0 +1,33 @@
+# Polygon Manager
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)]()
+
+## Features
+
+- Create a dictionary containing polygon points
+- View drawn polygons
+- Get polygon points and use it anywhere
+
+
+## Tech
+
+Polygon Manager only uses a number of open source projects to work properly:
+
+- [Numpy] 
+- [OpenCV] 
+
+And of course Polygon Manager itself is open source with a [https://github.com/memona008/Polygon_Manager][poly]
+ on GitHub.
+
+## Installation
+
+Install the package with entering this command in command line interface
+
+```sh
+pip install polygon_manager 
+or 
+pip3 install polygon_manager
+```
+
+   [poly]: <https://github.com/memona008/Polygon_Manager>
+
---
+++ setup.py
@@ -0,0 +1,38 @@
+from distutils.core import setup
+setup(
+  name = 'polygon_manager',
+  packages = ['polygon_manager'],
+  version = '0.1',
+  license='MIT',
+  description = 'It helps in drawing polygon on video frame and save it in pkl file. You can create,edit,view the polygon dictionary',
+  author = 'Memona S',
+  author_email = 'memonasultan54@gmail.com',
+  url = 'https://github.com/memona008/Polygon_Manager',
+  download_url = 'https://github.com/memona008/Polygon_Manager/archive/refs/tags/v_01.tar.gz',
+  keywords = ['polygon', 'video', 'draw polygon', 'box', 'parallelogram', 'image', 'manager', 'capture','points'],
+  install_requires=[
+          'numpy',
+          'opencv-python',
+      ],
+  classifiers=[  # Optional
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 3 - Alpha',
+
+    # Indicate who your project is intended for
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+
+    # Pick your license as you wish
+    'License :: OSI Approved :: MIT License',
+
+    # Specify the Python versions you support here. In particular, ensure
+    # that you indicate whether you support Python 2, Python 3 or both.
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
